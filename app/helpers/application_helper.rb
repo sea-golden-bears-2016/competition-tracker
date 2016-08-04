e5c41b8315ec71e1d45128000f0d279616fce76d
Andrew Peterson <email": null>
2016-08-04 10:59:38 -0700

    Add css for tables and logic to populate tables with dashes.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  # need a method that checks match results for a specific round
+    # returns competitor name for winner
+    # returns competitor name for loser
+    # returns arbitrary character if match not yet played
+
+
+
 end
--- app/models/match.rb
+++ app/models/match.rb
@@ -5,5 +5,10 @@ class Match < ApplicationRecord
   belongs_to :winner, class_name: :Competitor
   belongs_to :loser, class_name: :Competitor
 
+  scope :from_round, ->(round_number) { where(round_number: round_number) }
+
+
   validates_presence_of :league
+
+  attr_reader :winner
 end
