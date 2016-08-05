8ae29a488b7b5b9221a40409ced1694f9505a491
Jacob Rapp <email": null>
2016-08-04 18:17:51 -0700

    Fix tests to log in admin

--- spec/admin/competitor_spec.rb
+++ spec/admin/competitor_spec.rb
@@ -3,6 +3,17 @@ require 'rails_helper'
 feature "admin competitor update" do
   let!(:league1) { League.create(name: "League 1") }
   let!(:competitor) { Competitor.create(name: "Jordan Sillypants", league_id: "#{league1.id}") }
+  before(:each) do
+    User.create(
+      username: 'admin',
+      password: 'secret',
+    )
+
+    visit 'admin/session/new'
+    fill_in 'Username', with: 'admin'
+    fill_in 'Password', with: 'secret'
+    click_button 'Log In'
+  end
 
   it 'can add a competitor to a given league' do
     visit admin_league_path(league1)
--- spec/admin/league_spec.rb
+++ spec/admin/league_spec.rb
@@ -3,9 +3,20 @@ require 'rails_helper'
 feature "admin league update" do
   let!(:league1) { League.create(name: "League 1") }
   let!(:league2) { League.create(name: "League 2") }
+  before(:each) do
+    User.create(
+      username: 'admin',
+      password: 'secret',
+    )
+
+    visit 'admin/session/new'
+    fill_in 'Username', with: 'admin'
+    fill_in 'Password', with: 'secret'
+    click_button 'Log In'
+  end
 
   it 'can change the name of the league' do
-    visit 'admin/dashboard'
+    visit admin_dashboard_path
 
     click_button 'Update League 1'
 
