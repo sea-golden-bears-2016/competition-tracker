021f445e20b2dbfd669e304a77ec372d37ab19c1
Jacob Rapp <email": null>
2016-08-05 12:48:56 -0700

    Add current_leader method to league model, displays on dashboard, seed file creates matches

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,39 @@
-League.create(name: 'League A')
-League.create(name: 'League B')
+Match.delete_all
+Competitor.delete_all
+League.delete_all
+User.delete_all
 
-4.times do
-  competitor = Competitor.new({
+league1 = League.create(name: 'League A')
+league2 = League.create(name: 'League B')
+
+6.times do
+  league1.competitors.create({
     :name => Faker::Name.name,
-    :wins_count => 0,
-    :loses_count => 0,
-    :league_id => 1
   })
-  competitor.save
 end
 
-4.times do
-  competitor = Competitor.new({
+6.times do
+  league2.competitors.create({
     :name => Faker::Name.name,
-    :wins_count => 0,
-    :loses_count => 0,
-    :league_id => 2
   })
-  competitor.save
+end
+
+10.times do
+  match = league1.matches.new
+  match.round_number = rand(4)
+  match.competitors = league1.competitors.sample(2)
+  match.winner_id = match.competitors[0].id
+  match.loser_id = match.competitors[1].id
+  match.save
+end
+
+10.times do
+  match = league2.matches.new
+  match.round_number = rand(4)
+  match.competitors = league2.competitors.sample(2)
+  match.winner_id = match.competitors[0].id
+  match.loser_id = match.competitors[1].id
+  match.save
 end
 
 User.create(username: "admin", password: "secret")
