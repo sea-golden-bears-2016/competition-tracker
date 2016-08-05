471d14757b6f5c33de936bef5d0c7f902b546800
Jacob Rapp <email": null>
2016-08-05 13:28:42 -0700

    Comment out match seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,23 +18,23 @@ end
   })
 end
 
-10.times do
-  match = league1.matches.new
-  match.round_number = rand(4)
-  match.competitors = league1.competitors.sample(2)
-  match.winner_id = match.competitors[0].id
-  match.loser_id = match.competitors[1].id
-  match.save
-end
+# 10.times do
+#   match = league1.matches.new
+#   match.round_number = rand(4)
+#   match.competitors = league1.competitors.sample(2)
+#   match.winner_id = match.competitors[0].id
+#   match.loser_id = match.competitors[1].id
+#   match.save
+# end
 
-10.times do
-  match = league2.matches.new
-  match.round_number = rand(4)
-  match.competitors = league2.competitors.sample(2)
-  match.winner_id = match.competitors[0].id
-  match.loser_id = match.competitors[1].id
-  match.save
-end
+# 10.times do
+#   match = league2.matches.new
+#   match.round_number = rand(4)
+#   match.competitors = league2.competitors.sample(2)
+#   match.winner_id = match.competitors[0].id
+#   match.loser_id = match.competitors[1].id
+#   match.save
+# end
 
 User.create(username: "admin", password: "secret")
 
